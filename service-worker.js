6df79ff6690888098a0f9a33141f3973a8e9959a
Mary Braunger <maryjezek@aol.com>
2022-03-31 20:46:47 -0400

    service worker

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,22 @@
-const APP_PREFIX = 'FoodEvent-';     
+const APP_PREFIX = 'Budget-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
-  "./public/index.html",
-  "./public/css/styles.css",
+  "./index.html",
+  "./css/styles.css",
   "./js/index.js",
   "./js/idb.js",
-
+  "./manifest.json",
+  "./css/style.css",
+  "./js/loadImages.js",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
 ];
 
 // Respond with cached resources
